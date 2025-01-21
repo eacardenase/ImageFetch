72dca5b0a751d8df5a2b78aa540f4f82b82bc87b
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-20 21:09:43 -0500

    Initial Commit

---
+++ ImageFetch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImageFetch
+//
+//  Created by Edwin Cardenas on 20/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
